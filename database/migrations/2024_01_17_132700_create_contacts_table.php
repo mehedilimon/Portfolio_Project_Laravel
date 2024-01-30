f8acaecb703d84e69552f075a60c7fc8f6b8b898
MehediLimon <mehedilimon1@gmail.com>
2024-01-30 22:05:40 +0600

    Initial commit

---
+++ database/migrations/2024_01_17_132700_create_contacts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->string(column:'fullName', length: 100);
+            $table->string(column:'email', length: 50);
+            $table->string(column:'phone', length: 50);
+            $table->text(column:'message');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contacts');
+    }
+};
